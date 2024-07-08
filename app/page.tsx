e6d1ee16fc4e8cb0646e52772c45fd5ed4e45f09
Shashank Kumar Gupta <91917475+shashank-2310@users.noreply.github.com>
2024-07-09 00:23:22 +0530

    Particle background fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
-"use client"
 import Hero from "@/components/shared/Hero";
 import Particles from "@/components/magicui/particles";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const { theme } = useTheme();
-  const [color, setColor] = useState("#ffffff");
 
-  useEffect(() => {
-    setColor(theme === "dark" ? "#ffffff" : "#000000");
-  }, [theme]);
   return (
     <main className="flex relative overflow-hidden z-10 flex-col items-center justify-between py-2 px-4 sm:p-10 lg:p-12 bg-background dark:text-white text-black">
       <Particles
         className="absolute inset-0 -z-0 hidden dark:block"
         quantity={200}
         ease={80}
-        color={color}
+        color={"#ffffff"}
         refresh
       />
       <Hero />
